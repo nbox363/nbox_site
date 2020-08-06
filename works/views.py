f0b0754122ab6c116ba9f3587b93a66ac6a27774
nbox363 <overchukdo@gmail.com>
2020-08-06 15:45:51 +0500

    add works

---
+++ works/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+
+# def works(request):
+#     return render(request, 'works/works_page.html')
+
+def works(request):
+    return HttpResponse('Here is will be my works')
