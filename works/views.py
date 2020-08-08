eb19dac8c99919179db4859e35d8d63f6aab8ff5
nbox363 <overchukdo@gmail.com>
2020-08-08 14:50:07 +0500

    add work page and change wrapper

--- works/views.py
+++ works/views.py
@@ -2,8 +2,8 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 
-# def works(request):
-#     return render(request, 'works/works_page.html')
-
 def works(request):
-    return HttpResponse('Here is will be my works')
+    return render(request, 'home/works_page.html')
+
+# def works(request):
+#     return HttpResponse('Here is will be my works')
