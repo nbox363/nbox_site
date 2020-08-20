4e9a5ecc24c1ddb06b15bc804f582a9ce94e659d
nbox363 <overchukdo@gmail.com>
2020-08-20 14:44:30 +0300

    add change password form

--- members/urls.py
+++ members/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
-from .views import UserRegisterView
+from .views import UserRegisterView, UserEditView, PasswordsChangeView
+from django.contrib.auth import views as auth_views
+
 
 urlpatterns = [
     path('register/', UserRegisterView.as_view(), name='register'),
+    path('edit_profile/', UserEditView.as_view(), name='edit_profile'),
+    path('password/', PasswordsChangeView.as_view()),
 ]
--- members/views.py
+++ members/views.py
@@ -1,12 +1,31 @@
-from django.views.generic import CreateView
+from django.views.generic import CreateView, UpdateView
 from django.urls import reverse_lazy
+from django.contrib.auth.views import PasswordChangeView
 
-from .forms import SignUpForm
+from .forms import SignUpForm, EditProfileForm, PasswordChangingForm
+
+
+class PasswordsChangeView(PasswordChangeView):
+    form_class = PasswordChangingForm
+    template_name = 'registration/change_password.html'
+    success_url = reverse_lazy('home')
 
 
 class UserRegisterView(CreateView):
     form_class = SignUpForm
     template_name = 'registration/registration.html'
-    success_url = reverse_lazy('login')
+    success_url = reverse_lazy('home')
+
+
+class UserEditView(UpdateView):
+    form_class = EditProfileForm
+    template_name = 'registration/edit_profile.html'
+    success_url = reverse_lazy('home')
+
+    def get_object(self, queryset=None):
+        return self.request.user
+
+
+
 
 
